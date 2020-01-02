d11d936093a13f6db6dd5c342054f3987af209d4
takahiro.sawakami <takahirosawakami81@gmail.com>
2020-01-02 17:07:29 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand navbar-dark" style="background-color:#7b5544">
+        <div class="container">
+            <a class="navbar-brand" href="/">Cafelist</a>
+            <div class="navbar-nav mr-auto"></div>
+            <div class="navbar-nav">
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">ゲスト</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li class="dropdown-item"><a href="#">ログイン</a></li>
+                        <li class="dropdown-item"><a href="#">アカウント登録</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
