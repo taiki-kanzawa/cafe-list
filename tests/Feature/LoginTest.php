9ccf8252d6d9b50d7cfa9b0f06a59e00447640b3
takahiro.sawakami <takahirosawakami81@gmail.com>
2020-12-24 11:41:19 +0000

    ログインのテスト

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,16 +5,49 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class LoginTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    // ログイン画面の表示のテスト
+    public function testGetLoginPage()
     {
-        $this->assertTrue(true);
+        $response = $this->get('login');
+        $response->assertStatus(200);
+    }
+    
+    // ログインエラーのテスト
+    public function testLoginError()
+    {
+        // DBに保存されていないデータでログインを試みる
+        $response = $this->post('login', [
+            'email' => 'test@test.com',
+            'password' => 'test01',
+        ]);
+        
+        // 認証されていないか確認
+        $this->assertGuest();
+    }
+    
+    
+    // ログイン成功のテスト
+    public function testLoginSuccess()
+    {
+        // ユーザーを作成
+        $user = factory(User::class)->create([
+            'password' => bcrypt('test01')
+        ]);
+        
+        // 作成したユーザーでログイン実行
+        $response = $this->post('login', [
+            'email' => $user->email,
+            'password' => 'test01'
+        ]);
+        
+        // 認証されているか確認
+        $this->assertAuthenticated();
+        
+        // トップページにリダイレクトされたか確認
+        $response->assertRedirect('/');
     }
 }
